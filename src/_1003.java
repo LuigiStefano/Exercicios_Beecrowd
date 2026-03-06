d93611935b740044f6ec73fd430ac3ed7fac44f1
LuigiStefano <luigi.devdesign@gmail.com>
2026-03-06 17:41:06 -0300

    add: exercicio 1003

---
+++ src/_1003.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _1003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A,B, SOMA;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        SOMA = A + B;
+        System.out.println("SOMA = " + SOMA );
+        scanner.close();
+    }
+}
